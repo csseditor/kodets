42c334ba3d5984e48fcbce39d3f9abb67e7aa1ba
Tim Green <tiimgreen@gmail.com>
2014-06-14 23:09:56 +0100

    Add static pages and header and footer partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  get '/about',   to: 'pages#about',   as: :about
+  get '/pricing', to: 'pages#pricing', as: :pricing
+  get '/contact', to: 'pages#contact', as: :contact
 end
