c235694fb244691f5fa7b799ffdd4178c2f8d264
Tim Green <tiimgreen@gmail.com>
2014-06-15 17:42:49 +0100

    Improve header class helper to remove duplication

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,9 @@
 module PagesHelper
-  def clear_header?
-    controller.controller_name == 'pages' && controller.action_name == 'home'
+  def header_class(controller_name, action_name)
+    if controller.controller_name == 'pages' && controller.action_name == 'home'
+      'navbar navbar-inverse hero'
+    else
+      'navbar navbar-inverse normal'
+    end
   end
 end
