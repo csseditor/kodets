ede2188c35c287b0e15c12c3a1945feebd01fea6
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-06-25 20:38:31 +0100

    Generate Teachers model with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  devise_for :teachers
+
   get '/about',   to: 'pages#about',   as: :about
   get '/pricing', to: 'pages#pricing', as: :pricing
   get '/contact', to: 'pages#contact', as: :contact
