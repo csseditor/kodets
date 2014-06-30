942b53e70e7e2f2b599ae9f345243e5bf1cede76
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-06-30 17:17:51 +0100

    Add view layout for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    devise_controller? ? 'devise' : 'application'
+  end
 end
