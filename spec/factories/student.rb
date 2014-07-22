ad162d41172adb995a0d3210fa4d6df9d2baaa63
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-07-22 12:09:11 +0100

    Improve factory password definitions

--- spec/factories/student.rb
+++ spec/factories/student.rb
@@ -2,9 +2,8 @@ FactoryGirl.define do
   factory :student do
     name { Faker::Lorem.word }
     email { Faker::Internet.email }
-    password = Faker::Internet.password
-    password password
-    password_confirmation password
+    password { Faker::Internet.password }
+    password_confirmation { password }
     organisation { build(:organisation) }
   end
 end
--- spec/factories/teacher.rb
+++ spec/factories/teacher.rb
@@ -2,8 +2,7 @@ FactoryGirl.define do
   factory :teacher do
     username { Faker::Internet.user_name }
     email { Faker::Internet.email }
-    password = Faker::Internet.password
-    password password
-    password_confirmation password
+    password { Faker::Internet.password }
+    password_confirmation { password }
   end
 end
