6a6c7ff95740eb628486b343ccefeab8c075f1dc
Tim Green <tiimgreen@gmail.com>
2014-07-26 00:29:37 +0100

    Add first_name and last_name to views, add helper methods to Teacher, update devise error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   include ApplicationHelper
 
   protect_from_forgery with: :exception
@@ -6,6 +7,18 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :first_name, :last_name, :password,
+               :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :first_name, :last_name, :password,
+               :password_confirmation, :current_password)
+    end
+  end
 
   def layout_by_resource
     devise_controller? ? 'devise' : 'application'
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,8 +5,24 @@ class Teacher < ActiveRecord::Base
   has_one :organisation
   after_create :create_organisation
 
-  validates :username,  presence: true, on: :update
-  validates :email,     presence: true
+  validates :username,   presence: true, on: :create
+  validates :email,      presence: true
+  validates :first_name, presence: true, on: :create
+  validates :last_name,  presence: true, on: :create
+
+  def full_name
+    if self.first_name && self.last_name
+      first_name.capitalize + ' ' + last_name.capitalize
+    else
+      'Teacher'
+    end
+  end
+
+  def titlised_name
+    if self.title && self.last_name
+      self.title.capitalize + ' ' + self.last_name.capitalize
+    end
+  end
 
   private
 
