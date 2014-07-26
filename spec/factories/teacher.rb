cebac149c791bbb0f8b547471336eb7e7e4158a5
Tim Green <tiimgreen@gmail.com>
2014-07-26 01:08:07 +0100

    Improve specs

--- spec/factories/teacher.rb
+++ spec/factories/teacher.rb
@@ -2,6 +2,9 @@ FactoryGirl.define do
   factory :teacher do
     username { Faker::Internet.user_name }
     email { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    title { 'Mr.' }
     password { Faker::Internet.password }
     password_confirmation { password }
   end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,9 +1,10 @@
 RSpec.describe Teacher, type: :model do
   it { should have_one(:organisation) }
 
-  it { should validate_presence_of(:username).on(:update) }
-
+  it { should validate_presence_of(:username).on(:create) }
   it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:first_name).on(:create) }
+  it { should validate_presence_of(:last_name).on(:create) }
 
   it 'should create a new Organisation after creation' do
     teacher = create(:teacher)
