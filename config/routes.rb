1590dc1202c59ad7e5bd63ef56f31030df710193
Tim Green <tiimgreen@gmail.com>
2014-08-17 17:29:43 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
   # Code Lessons
   get '/tracks/:id/code-lessons/new', to: 'code_lessons#new',     as: :new_code_lesson
   get '/tracks/:id/code-lessons',     to: 'code_lessons#index',   as: :code_lessons
-  get '/code-lessons/:id',            to: 'code_lesson#show',     as: :code_lesson
+  get '/code-lessons/:id',            to: 'code_lessons#show',     as: :code_lesson
   get '/code-lessons/:id/edit',       to: 'code_lessons#edit',    as: :edit_code_lesson
   match '/code-lessons/:id',          to: 'code_lessons#destroy', as: :delete_code_lesson, via: :delete
   match '/code-lessons/:id',          to: 'code_lessons#update',                           via: :patch
