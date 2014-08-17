1a26ca7e3a44a0437cc15405bf5d8c50a4605f4c
Tim Green <tiimgreen@gmail.com>
2014-08-17 16:50:18 +0100

    Update CodeLesson controller

--- app/controllers/code_lessons_controller.rb
+++ app/controllers/code_lessons_controller.rb
@@ -1,10 +1,46 @@
 class CodeLessonsController < ApplicationController
+
   def new
+    @code_lesson = CodeLesson.new
   end
 
-  def show
+  def create
+    @track = Track.find params[:id]
+    @code_lesson = @track.code_lessons.build(code_lesson_params)
+    if @code_lesson.save
+      redirect_to edit_code_lesson_path @code_lesson
+    else
+      flash[:warning] = 'Problem creating Code Lesson'
+      redirect_to track_path @track
+    end
   end
 
   def edit
+    @code_lesson = CodeLesson.find params[:id]
+  end
+
+  def update
+    @code_lesson = CodeLesson.find params[:id]
+    if @code_lesson.update_attributes code_lesson_params
+      redirect_to code_lesson_path @code_lesson
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @code_lesson = CodeLesson.find params[:id]
+  end
+
+  def destroy
+    @code_lesson = CodeLesson.find params[:id]
+    @track = @code_lesson.track
+    if @code_lesson.destroy
+      flash[:success] = 'Code Lesson deleted'
+      redirect_to track_path @track
+    else
+      flash[:warning] = 'Problem deleting Code Lesson'
+      redirect_to track_path @track
+    end
   end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ActiveRecord::Base
   belongs_to :course
-  has_many :code_lessons
+  has_many :code_lessons, dependent: :destroy
 
   validates :name,        presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 250 }
