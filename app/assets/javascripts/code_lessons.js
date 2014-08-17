e9b2dcbeea350a7bcad0bb72c22484914af6a14d
Tim Green <tiimgreen@gmail.com>
2014-08-17 21:41:53 +0100

    Fix jQConsole error, update views

--- app/assets/javascripts/code_lessons.js
+++ app/assets/javascripts/code_lessons.js
@@ -1,30 +1,27 @@
-$(function () {
-  var jqconsole = $('#console').jqconsole('', '');
-  var startPrompt = function () {
-    // Start the prompt with history enabled.
-    jqconsole.Prompt(true, function (input) {
-      // Output input with the class jqconsole-output.
-      jqconsole.Write(input + '\n', 'jqconsole-output');
+if ($('#console').length) {
+  $(function() {
+    var jqconsole = $('#console').jqconsole('', '');
+    var startPrompt = function () {
+      // Start the prompt with history enabled.
+      jqconsole.Prompt(true, function (input) {
+        // Output input with the class jqconsole-output.
+        jqconsole.Write(input + '\n', 'jqconsole-output');
 
-      $(".jqconsole").animate({
-        scrollTop: $(".jqconsole").height()
-      }, 1000);
+        $(".jqconsole").animate({
+          scrollTop: $(".jqconsole").height()
+        }, 1000);
 
-      startPrompt();
-    });
-  };
-  startPrompt();
-});
+        startPrompt();
+      });
+    };
+    startPrompt();
+  });
+}
 
-$("#disable_date").click(function() {
+$("input#disable_date").click(function() {
+  alert('clicked');
   if (!this.checked) {
     $("#code_lesson_date_due").val("");
   }
   $("#code_lesson_date_due").attr("disabled", !this.checked);
 });
-
-// CodeLesson#edit Tabs
-$('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
