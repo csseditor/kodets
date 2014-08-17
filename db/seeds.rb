4527c5a33e54789ef5aaabacde06cc821936ad9e
Tim Green <tiimgreen@gmail.com>
2014-08-17 17:24:43 +0100

    Add Languages to seed data

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+def create_languages
+  languages = [["C", "c_cpp", "c"], ["C++", "c_cpp", "c++"], ["C#", "csharp", "csharp"],
+              ["HTML", "html", ""], ["JavaScript", "javascript", "javascript"],
+              ["Lua", "lua", "lua"], ["Markdown", "markdown"], ["PHP", "php", "php"],
+              ["Python 2", "python", "python2"], ["Python 3", "python", "python3"],
+              ["Ruby", "ruby", "ruby"]]
+
+  languages.each do |language|
+    Language.create(name: language[0], ace_slug: language[1], evalso_slug: language[2])
+  end
+end
+
+create_languages
