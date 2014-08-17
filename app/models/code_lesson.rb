abce37db3d8978b9b4a398d72aa71988eb470ded
Tim Green <tiimgreen@gmail.com>
2014-08-17 16:42:47 +0100

    Add helper methods to CodeLesson model

--- app/models/code_lesson.rb
+++ app/models/code_lesson.rb
@@ -1,3 +1,26 @@
 class CodeLesson < ActiveRecord::Base
   belongs_to :track
+
+  after_validation :make_visible
+
+  def to_param
+    [id, name.parameterize].join('-')
+  end
+
+  def visible?
+    visible
+  end
+
+  def shareable?
+    shareable
+  end
+
+  private
+
+    def make_visible
+      if name.present? && language_id.present? && lesson_content.present? &&
+        instructions.present? && order.present?
+        self.visible = true
+      end
+    end
 end
