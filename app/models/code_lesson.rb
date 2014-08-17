d1e78f8e969150e6f84e02fc03183fa8da6f1bcd
Tim Green <tiimgreen@gmail.com>
2014-08-17 17:22:27 +0100

    Add modal to partial, update CodeLesson validation

--- app/models/code_lesson.rb
+++ app/models/code_lesson.rb
@@ -3,6 +3,15 @@ class CodeLesson < ActiveRecord::Base
 
   after_validation :make_visible
 
+  validates :name,            presence: true, length: { maximum: 50 }
+  validates :lesson_content,  presence: true, on: :update
+  validates :instructions,    presence: true, on: :update
+  validates :organisation_id, presence: true
+  validates :language_id,     presence: true
+  validates :user_id,         presence: true
+  validates :track_id,        presence: true
+
+
   def to_param
     [id, name.parameterize].join('-')
   end
