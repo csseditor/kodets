fc8c3651b9df200b7a9c69dc01c88cf1e51644a5
Tim Green <tiimgreen@gmail.com>
2014-08-20 17:14:40 +0100

    Dynamically set height of HTML/Markdown iframe to 40% and all other languages console to 300px

--- app/assets/javascripts/jqconsole.js
+++ app/assets/javascripts/jqconsole.js
@@ -3,9 +3,11 @@ $(function() {
   var startPrompt = function () {
     // Start the prompt with history enabled.
     jqconsole.Prompt(true, function (input) {
+      
       // Output input with the class jqconsole-output.
       jqconsole.Write(input + '\n', 'jqconsole-output');
 
+      // Scrolls to bottom when content goes of page
       $(".jqconsole").animate({
         scrollTop: $(".jqconsole").height()
       }, 1000);
