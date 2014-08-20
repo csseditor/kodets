3e87fd509f573571ffb8e808b8ee9a66ee91a061
Tim Green <tiimgreen@gmail.com>
2014-08-20 20:40:35 +0100

    Add self-calculating order when creating new CodeLessons

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,15 @@
 module TracksHelper
+
+  # Find other lessons in the current track
+  def find_lessons_in_same_track(track)
+    @other_lessons = CodeLesson.where('track_id = ?', track.id)
+                     # VideoLesson.where('track_id = ?', track.id) +
+                     # Quiz.where('track_id = ?', track.id)
+  end
+
+  # Generates order for new lesson in track
+  def find_order
+    find_lessons_in_same_track Track.find(params[:id])
+    @other_lessons.present? ? @other_lessons.map(&:order).max + 1 : 1
+  end
 end
