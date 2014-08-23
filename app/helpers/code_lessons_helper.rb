61471509f4389a5c8320ce45cc80fcacff63bbab
Tim Green <tiimgreen@gmail.com>
2014-08-23 17:09:51 +0100

    Tell user what fields they need to add to make CodeLesson public

--- app/helpers/code_lessons_helper.rb
+++ app/helpers/code_lessons_helper.rb
@@ -1,2 +1,14 @@
 module CodeLessonsHelper
+
+  # Returns a human-readable sentence of missing fields needed for the lesson
+  # to be public
+  def required_empty_fields(code_lesson)
+    fields = %w[name language_id lesson_content instructions order]
+    missing = []
+    fields.each do |field|
+      missing.push(field.humanize) if code_lesson.read_attribute(field) == "" || code_lesson.read_attribute(field) == nil
+    end
+
+    missing.to_sentence if missing.any?
+  end
 end
