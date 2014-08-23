fffd8fce1efdf908c10de07ad01c71cb46f995fa
Tim Green <tiimgreen@gmail.com>
2014-08-23 19:59:12 +0100

    Move js to jqconsole file to get access to jqconsole

--- app/assets/javascripts/jqconsole.js
+++ app/assets/javascripts/jqconsole.js
@@ -3,7 +3,7 @@ $(function() {
   var startPrompt = function () {
     // Start the prompt with history enabled.
     jqconsole.Prompt(true, function (input) {
-      
+
       // Output input with the class jqconsole-output.
       jqconsole.Write(input + '\n', 'jqconsole-output');
 
@@ -17,3 +17,27 @@ $(function() {
   };
   startPrompt();
 });
+
+$('a#javascript-run-code').on('click', function(e) {
+  e.preventDefault();
+  evaluateJavascript(editor.getSession().getValue());
+});
+
+function evaluateJavascript(code) {
+  var console = {
+    log: function (item) {
+      jqconsole.Write(item + '\n');
+    },
+    warn: function (item) {
+      jqconsole.Write(item + '\n', 'jqconsole-warning');
+    },
+    info: function (item) {
+      jqconsole.Write(item + '\n', 'jqconsole-info');
+    },
+    error: function (item) {
+      jqconsole.Write(item + '\n', 'jqconsole-error');
+    }
+  };
+
+  eval(code);
+}
